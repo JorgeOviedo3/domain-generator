c1eaa420f58e39d118ddcca1b8675b13a6d4b54f
Jorge Oviedo <jorgeoviedo1999@gmail.com>
2022-09-02 00:18:58 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domains = [".com", ".net"];
+
+  function domainGenerator(pron, adjt, n, dom) {
+    let final = [];
+    for (let i = 0; i < pron.length; i++) {
+      for (let j = 0; j < adjt.length; j++) {
+        for (let k = 0; k < n.length; k++) {
+          for (let l = 0; l < dom.length; l++) {
+            final.push(
+              pron[i]
+                .concat(adjt[j])
+                .concat(n[k])
+                .concat(dom[l])
+            );
+          }
+        }
+      }
+    }
+    return final;
+  }
+
+  console.log(domainGenerator(pronoun, adj, noun, domains));
 };
